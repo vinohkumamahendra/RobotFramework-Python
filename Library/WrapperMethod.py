90cda05ea48443874f99dceeb8553899df6629fc
vinohkumamahendra <Vinothkumarmahendra@gmail.com>
2018-02-26 20:07:49 +0530

    Update WrapperMethod.py

--- Library/WrapperMethod.py
+++ Library/WrapperMethod.py
@@ -1,5 +1,7 @@
 from Selenium2Library import Selenium2Library
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from robot.api.deco import keyword
 from xlrd import open_workbook
 import time
@@ -8,6 +10,9 @@ import xlrd
 from Pojo.TracTicket import TracTicket
 from selenium.webdriver.support.ui import Select
 from GoogleDriveFile import ReadGoogleSheet
+from selenium.webdriver.common.by import By
+from decimal import *
+from selenium import webdriver
 
 class WrapperMethod(Selenium2Library):
     @keyword('Open Seperate Browser')
@@ -71,7 +76,7 @@ class WrapperMethod(Selenium2Library):
                         picklist =True;
             if picklist:
                tractk = TracTicket(sheet.cell_value(row_index,alert_ticket_ticketID) + "-" + sheet.cell_value(row_index,alert_ticket_Description)
-                                    , sheet.cell_value(row_index,alert_ticket_Priority), first_day.date(), last_day.date(), 6, release, 1, "vinoth.mahendira", sheet.cell_value(row_index,alert_ticket_WH), "Test Execution")
+                                    , sheet.cell_value(row_index,alert_ticket_Priority), first_day.date(), last_day.date(), 6, release, 1, "vinoth.mahendira", sheet.cell_value(row_index,alert_ticket_WH), "Test Case Execution")
                TrackTickets.append(tractk)
 
         for item in TrackTickets:
@@ -83,7 +88,9 @@ class WrapperMethod(Selenium2Library):
 
     @keyword('Create Ticket')
     def create_ticket(self,tractk):
+        delay =10;
         driver = self._current_browser();
+        driver.get("https://prism.aspiresys.com/Narvar/ticket/1992")
         select = Select(driver.find_element_by_id('field-type'))
         select.select_by_visible_text('Maintenance')
         driver.find_element_by_id('field-summary').send_keys(tractk.name)
@@ -104,5 +111,45 @@ class WrapperMethod(Selenium2Library):
         select.select_by_visible_text(tractk.owner)
         driver.find_element_by_xpath("//*[@value='Create ticket']").click()
         print("Title of the page ", driver.title)
+        assert (tractk.name in driver.title)
+        tractk.trac_id = driver.current_url.replace("https://prism.aspiresys.com/Narvar/ticket/", "")
+        print("Trac id",tractk.trac_id)
+        driver.find_element_by_id("estimatedhours-" + tractk.trac_id).click();
+        WebDriverWait(driver,delay).until(EC.visibility_of_element_located((By.XPATH, "//div[@aria-labelledby='ui-dialog-title-estimated-hours-dialog-list'][contains(@style,'block')]")))
+        WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.ID,"#add-new")))
+        driver.find_element_by_id("#add-new").click()
+        WebDriverWait(driver, delay).until(EC.visibility_of_element_located((By.XPATH,
+                                                                             "//div[@aria-labelledby='ui-dialog-title-estimated-hours-dialog-entry'][contains(@style,'block')]")))
+        WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.ID,"timesheet_hours")))
+        time = Decimal(tractk.time)
+        hour = int(time)
+        minute = str(int((time-hour)*60)).zfill(2)
+        driver.find_element_by_id("timesheet_hours").clear()
+        driver.find_element_by_id("timesheet_hours").send_keys(str(hour))
+        select = Select(driver.find_element_by_id('timesheet_minutes'))
+        select.select_by_visible_text(minute)
+        select = Select(driver.find_element_by_id('timesheet_reason'))
+        select.select_by_visible_text(tractk.task)
+        driver.find_element_by_id("timesheet_comments").send_keys(tractk.task)
+        driver.find_element_by_id("save").click()
+        WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH,"//*[text()='"+ tractk.task.lower() +"']/following-sibling::*[text()='" + tractk.task + "']")))
+        WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, "//a[.='close'][ancestor::div[contains(@style,'block')]]")))
+        driver.find_element_by_xpath("//a[.='close'][ancestor::div[contains(@style,'block')]]").click()
+        estimated_hour = driver.find_element_by_id("estimatedhours-" + tractk.trac_id).text
+        print("Calculated Estimated Hour ", estimated_hour)
+        assert (estimated_hour == (str(hour).zfill(2) + ":" + minute))
 
-
+    @keyword('Go to saleforce')
+    def new_click(self):
+        driver = self._current_browser();
+        driver.get("https://ssodisney--kanaqa.cs90.my.salesforce.com")
+        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH,
+                                                                             "//em[@class='x-btn-split']")))
+        element =  driver.find_element_by_xpath("//em[@class='x-btn-split']")
+        print("x",element.location)
+        print("x", element.location["x"])
+        print("y", element.size)
+        action = webdriver.ActionChains(driver)
+        action.move_to_element_with_offset(element, 235, 0)
+        action.click()
+        action.perform()
